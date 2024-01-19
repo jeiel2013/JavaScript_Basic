a4bf9f488fa39c6980a91bc8f6b562e03d286793
jeiel2013 <contatojeiel2013@gmail.com>
2024-01-19 17:36:20 -0300

    19_01_23

---
+++ aula16/index.js
@@ -0,0 +1,44 @@
+//               01234566789
+// const nome = 'Luiz Otávio'
+// console.log(nome[0])
+
+//               0       1        2
+const alunos = ['Luiz', 'Maria', 'João']
+
+console.log(typeof alunos) // object
+console.log(alunos instanceof Array) // mostra se realmente está trabalhando com array
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+// console.log(alunos.slice(0,-1))
+
+// console.log(alunos[50]) // undefined
+
+// delete alunos[1] // o índice 1 fica vazio
+// console.log(alunos)
+
+// const removido = alunos.pop() // remover o último
+// const removido = alunos.shift() // remnover do começo
+// console.log(removido)
+
+// console.log(alunos)
+
+
+// alunos.unshift('Luzia') // Adiciona no começo
+// alunos.unshift('Fábio')
+
+
+// alunos.push('Luiza') // Adicio0na no fim do array
+// alunos.push('Fábio')
+
+
+// alunos[alunos.length] = 'Luiza' // Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' // Altera
+// alunos[3] = 'Luiza'  // Adicionando
+
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[2])
